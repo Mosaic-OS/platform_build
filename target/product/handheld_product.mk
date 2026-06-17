1744a05660cc2b46e41d0ae75128dbe569278ab5
SuperNeatJaguar <293907574+SuperNeatJaguar@users.noreply.github.com>
2026-06-18 01:19:09 +0200

    Add MosaicOS changes
    - Removed/renamed apps
    - Add custom font & overlay path

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -19,21 +19,20 @@
 # it definitely doesn't belong on other types of devices (if it
 # does, use base_product.mk).
 $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
+$(call inherit-product, vendor/mosaic/mosaic.mk)
 
 # /product packages
 PRODUCT_PACKAGES += \
     AppStore \
-    Auditor \
-    Calendar \
     Camera \
     Contacts \
     DeskClock \
     ExactCalculator \
-    Gallery2 \
+    Glimpse \
     InfoApp \
     LatinIME \
     Music \
-    PdfViewerGOS \
+    PdfViewer \
     preinstalled-packages-platform-handheld-product.xml \
     SettingsIntelligence \
     SpeechServices \
